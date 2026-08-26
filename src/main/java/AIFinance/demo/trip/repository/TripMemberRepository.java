2fcc8c82c4e8b583bda9bd383037181a8a1c9e2b
Lee SeungJoon <sj020521@naver.com>
2026-08-26 17:30:30 +0900

    LEFT 추가 및 조회용 메서드 추가

--- src/main/java/AIFinance/demo/trip/repository/TripMemberRepository.java
+++ src/main/java/AIFinance/demo/trip/repository/TripMemberRepository.java
@@ -4,7 +4,18 @@ import AIFinance.demo.trip.entity.TripMember;
 import AIFinance.demo.trip.entity.enums.TripMemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TripMemberRepository extends JpaRepository<TripMember, Long> {
     // 여행방 Id, 유저 Id, 멤버 상태 확인
     boolean existsByTrip_IdAndUser_IdAndStatus(Long tripId, Long userId, TripMemberStatus status);
+
+    Optional<TripMember> findByTrip_IdAndUser_IdAndStatus(Long tripId, Long userId, TripMemberStatus status);
+
+    List<TripMember> findAllByTrip_IdAndStatus(Long tripId, TripMemberStatus status);
+
+    int countByTrip_IdAndStatus(Long tripId, TripMemberStatus status);
+
+    List<TripMember> findAllByUser_IdAndStatus(Long userId, TripMemberStatus status);
 }
