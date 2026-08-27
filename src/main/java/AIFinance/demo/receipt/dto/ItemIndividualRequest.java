3736d569528016b920d0fd6bc8517f39415c3c8a
bsh-ko <ssuh3872@gmail.com>
2026-08-27 22:20:46 +0900

    feat: 전액부담 1인 요청 dto

---
+++ src/main/java/AIFinance/demo/receipt/dto/ItemIndividualRequest.java
@@ -0,0 +1,11 @@
+package AIFinance.demo.receipt.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ItemIndividualRequest {
+
+    private Long tripMemberId;
+}
